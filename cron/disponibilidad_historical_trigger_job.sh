e241b574cbb424612ea1771eb971f3b58c7de8ec
へすす  あるべると Baizabal <ambagasdowa@gmail.com>
2021-11-04 12:30:35 -0600

    Add Job for historicarl in kml/cron add to cron job example 0 23 * * * /var/www/kml.uruk/public_html/kml/cron/disponibilidad_historical_trigger_job.sh

---
+++ cron/disponibilidad_historical_trigger_job.sh
@@ -0,0 +1,33 @@
+
+#ip=192.168.20.235
+ip=10.8.0.235
+usr='zam'
+pass='lis'
+database='gstdb'
+
+
+#sqsh -S SERV -U user -P passwd -D db -L bcp_colsep=',' -m bcp 
+#	-C 'select * from some_table where foo=bar' > /path/to/output.out
+
+# Define table variable
+
+sqsh -S $ip -U $usr -P $pass -D $database -L bcp_colsep=' ' -m bcp -C " insert into 
+                                                                        	sistemas.dbo.disponibilidad_tbl_hist_unidades_gst_indicators
+                                                                        	select
+                                                                        				 unidad, id_flota, flota, xid_status, xestatus, id_status, estatus, tipo_status, clasification_name
+                                                                        				,group_name, operador, remolque, id_area, area, id_tipo_operacion, segmento, description, compromise
+                                                                        				,status_viaje, desc_viaje, status_taller, desc_taller, units_type, 0 --as 'iseditable'
+                                                                        				, id_area_taller, area_taller
+                                                                        				,id_orden, fecha_inicio,fecha_ingreso, fecha_prometida
+                                                                        				,lastTrip
+                                                                        				,id_remolque1
+                                                                        				,id_remolque2
+                                                                        				,id_dolly
+                                                                        				,f_despachado
+                                                                        				,StatusDays
+                                                                        				,convert(nvarchar(max),current_timestamp,23) --'2021-08-10' --created
+                                                                        				,1 -- status
+                                                                        	from
+                                                                        				sistemas.dbo.disponibilidad_currentview_rpt_unidades_gst_indicators
+                                                                        --	where 
+                                                                        --				created =  convert(nvarchar(max),current_timestamp,23)"  
